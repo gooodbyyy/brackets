fbd5bac9952adbcf160ec4a54c908a6589f7c22c
geowashto <63078138+geowashto@users.noreply.github.com>
2021-08-24 18:16:24 +0300

    solve a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(string, bracketsConfig) {
+    string = string.replace(/[^{(|\[\]||)}]/g, "");
+    while (/(\(\)|\{\}|\[\]|\|\|)/.test(string)) string = string.replace("{}", "").replace("[]", "").replace("()", "").replace("||", "");
+    return string == ""
 }
